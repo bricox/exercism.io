01d288a74305fe12195b13d947105575951de14e
Brian Cox <bricox1@gmail.com>
2014-10-23 20:33:57 -0500

    Added phrase.rb

---
+++ ruby/word-count/phrase.rb
@@ -0,0 +1,14 @@
+class Phrase
+	attr_accessor :word_count;
+
+	def initialize(phrase)
+		@word_count = Hash.new
+		@word_count.default = 0
+
+		phrase = phrase.downcase.gsub(/[^a-z0-9'\s]/i, ' ')
+
+		phrase.split(' ').each do |word|
+			@word_count[word] += 1
+		end
+	end
+end
